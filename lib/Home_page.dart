8d9671e755a257306b5be62825c1a2e8accc8c5e
Dhvani <dhvanidhuliya8@gmail.com>
2022-01-26 22:33:53 +0530

    Day2 completed

---
+++ lib/Home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    final  int days = 30;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Demo),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome To $days Days Of Flutter"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
