63dc463fb41d20b6397566a5ade0d3b85a0307fe
Dhvani <dhvanidhuliya8@gmail.com>
2022-01-27 15:43:37 +0530

    Day4 completed

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(Demo),
+        title: Text("Demo"),
         ),
         body: Center(
           child: Container(
